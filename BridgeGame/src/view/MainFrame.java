5448dc22e4536a2a3aa986b6712d45aa9657cb42
jeonghoon <shb03323@naver.com>
2022-06-01 02:05:54 +0900

    [FEAT] add player num in PlayerScoreBoard

--- BridgeGame/src/view/MainFrame.java
+++ BridgeGame/src/view/MainFrame.java
@@ -1,11 +1,34 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
         setTitle("Bridge Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPanel();
+
+        setWindow();
+    }
+
+    private void setWindow() {
         setSize(800, 800);
+        setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private void setPanel() {
+        Container container = this.getContentPane();
+
+        // TODO : change playerNum to user input
+        int playerNum = 4;
+
+        // set border layout
+        JPanel panel = new JPanel(new BorderLayout(20, 20));
+
+        panel.add(new PlayerScoreBoard(playerNum), BorderLayout.WEST);
+
+        container.add(panel);
     }
 }
--- BridgeGame/src/view/PlayerScoreBoard.java
+++ BridgeGame/src/view/PlayerScoreBoard.java
@@ -1,7 +1,18 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PlayerScoreBoard extends JPanel {
+    public PlayerScoreBoard(int playerNum) {
+        this.setLayout(new GridLayout(playerNum, 1));
 
+        for (int i = 0; i < playerNum; i++) {
+            this.add(new JButton());
+        }
+    }
+
+    private void setSinglePlayerInfo() {
+
+    }
 }
